2db08749dadd45c53a13fbc52d0b1fd1141f89d7
Pataco80 <pataco80@gmail.com>
2017-11-16 18:22:51 +0100

    finalisation du menu, fin session 64

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,12 +3,23 @@
   
   var btn = document.querySelector('.header-nav__hamburgger');
   var html = document.querySelector('html');
+  var menu = document.querySelector('#mainMenu')
   var menOpened = false;
+  var classMenu = 'menu-opened';
+  
+  html.addEventListener('click', function(e){
+    console.log(this);
+    console.log(e.target);
+    
+    if(e.target === html && menOpened){
+      closeMenu();
+    }
+  })
   
   btn.addEventListener('click', toggleMenu);
   
   function toggleMenu(e){
-    if(menOpened) {
+    if(menOpened){
       closeMenu();
     } else {
       openMenu();
@@ -17,10 +28,16 @@
   
   function closeMenu(){
     menOpened = false;
-    html.classList.remove('menu-opened');
+    html.classList.remove(classMenu);
+    btn.blur();
+    menu.setAttribute('aria-expanded', false);
+    btn.setAttribute('aria-expanded', false);
   }
+  
   function openMenu(){
     openMenu = true;
-    html.classList.add('menu-opened');
+    html.classList.add(classMenu);
+    menu.setAttribute('aria-expanded', true);
+    btn.setAttribute('aria-expanded', true);
   }
 }())
